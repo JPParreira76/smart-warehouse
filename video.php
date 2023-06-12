2988543cc2bfa4b32b3600c051071088a53eb1d2
JPParreira76 <joao_parreira@mail.telepac.pt>
2023-06-12 22:47:43 +0100

    refresh

--- video.php
+++ video.php
@@ -16,7 +16,6 @@ if (!isset($_SESSION["username"])) {
 
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta http-equiv="refresh" content="10">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Video do Projeto</title>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-KK94CHFLLe+nY2dmCWGMq91rCGa5gtU4mk92HdvYe+M/SXH301p5ILy+dN9+nJOZ" crossorigin="anonymous">
