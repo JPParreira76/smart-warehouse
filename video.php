db88e79912ada5abb3a0f2320b973c4a70ad0111
JPParreira76 <joao_parreira@mail.telepac.pt>
2023-06-12 22:46:19 +0100

    video

--- video.php
+++ video.php
@@ -64,7 +64,7 @@ if (!isset($_SESSION["username"])) {
         <div class="card boxes">
             <div class="card-body">
                 <video controls>
-                    <source src="video/20230608_222144.mp4" type="video/mp4">
+                    <source src="video/video.mp4" type="video/mp4">
                     Your browser does not support the video tag.
                 </video>
             </div>
